77b0cc06acb63393006407227eb9850a2b3e2347
luis.berrocal.1942 <luis.berrocal.1942@600fe094-aafe-11de-8882-bb5941c2e707>
2010-06-29 00:51:53 +0000

    cambios en databaseexporter

--- src/main/java/org/gissolutions/jsimpleutils/sql/DatabaseExporter.java
+++ src/main/java/org/gissolutions/jsimpleutils/sql/DatabaseExporter.java
@@ -18,30 +18,57 @@ import org.dbunit.dataset.xml.FlatXmlDataSet;
 import org.dbunit.util.search.SearchException;
 
 public class DatabaseExporter {
-	public void export(Properties dbprops, String filename) throws ClassNotFoundException, SQLException, DataSetException, FileNotFoundException, IOException, SearchException {
+	
+	
+	public void export(Properties dbprops, String filename) throws  SQLException, DataSetException, FileNotFoundException, IOException, SearchException {
 		// database connection
-        Class driverClass = Class.forName("org.hsqldb.jdbcDriver");
-        Connection jdbcConnection = DriverManager.getConnection(
-                "jdbc:hsqldb:sample", "sa", "");
-        IDatabaseConnection connection = new DatabaseConnection(jdbcConnection);
+        loadDriver(dbprops.getProperty("driverClassName"));
+        String dbUrl = dbprops.getProperty("dbUrl");//"jdbc:hsqldb:sample";
+		String username = dbprops.getProperty("username");//"sa";
+		String password = dbprops.getProperty("password");//"";
+		IDatabaseConnection connection = buildConnection(dbUrl, username,
+				password);
 
         // partial database export
         QueryDataSet partialDataSet = new QueryDataSet(connection);
         partialDataSet.addTable("FOO", "SELECT * FROM TABLE WHERE COL='VALUE'");
         partialDataSet.addTable("BAR");
-        FlatXmlDataSet.write(partialDataSet, new FileOutputStream("partial.xml"));
+        FlatXmlDataSet.write(partialDataSet, new FileOutputStream(filename));
 
-        // full database export
-        IDataSet fullDataSet = connection.createDataSet();
-        FlatXmlDataSet.write(fullDataSet, new FileOutputStream("full.xml"));
-        
-        // dependent tables database export: export table X and all tables that
-        // have a PK which is a FK on X, in the right order for insertion
-        String[] depTableNames = 
-          TablesDependencyHelper.getAllDependentTables( connection, "X" );
-        
-        IDataSet depDataset = connection.createDataSet( depTableNames );
-        
-        FlatXmlDataSet.write(depDataset, new FileOutputStream("dependents.xml")); 
+//        // full database export
+//        IDataSet fullDataSet = connection.createDataSet();
+//        FlatXmlDataSet.write(fullDataSet, new FileOutputStream("full.xml"));
+//        
+//        // dependent tables database export: export table X and all tables that
+//        // have a PK which is a FK on X, in the right order for insertion
+//        String[] depTableNames = 
+//          TablesDependencyHelper.getAllDependentTables( connection, "X" );
+//        
+//        IDataSet depDataset = connection.createDataSet( depTableNames );
+//        
+//        FlatXmlDataSet.write(depDataset, new FileOutputStream("dependents.xml")); 
+	}
+
+	private IDatabaseConnection buildConnection(String dbUrl, String username,
+			String password)  {
+		IDatabaseConnection connection = null;
+		try {
+			Connection jdbcConnection = DriverManager.getConnection(dbUrl,
+					username, password);
+			connection = new DatabaseConnection(jdbcConnection);
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		return connection;
+	}
+
+	private void loadDriver(String driverClassName)  {
+		try {
+			Class.forName(driverClassName);
+		} catch (ClassNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 }
